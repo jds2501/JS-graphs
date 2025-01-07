1f816800416f79af8335588a6ffcf0d8d8907fb4
Jason Smith <jasondsmith2501@gmail.com>
2025-01-06 20:22:27 -0800

    Added solutions except DFS / BFS

--- graph.js
+++ graph.js
@@ -11,25 +11,56 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    vertexArray.forEach(element => {
+      this.nodes.add(element);
+    });
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    if (!v1.adjacent.has(v2)) {
+      v1.adjacent.add(v2);
+    }
+
+    if (!v2.adjacent.has(v1)) {
+      v2.adjacent.add(v1);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    if (v1.adjacent.has(v2)) {
+      v1.adjacent.delete(v2);
+    }
+
+    if (v2.adjacent.has(v1)) {
+      v2.adjacent.delete(v1);
+    }
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    if (this.nodes.has(vertex)) {
+      this.nodes.delete(vertex);
+
+      this.nodes.forEach(element => {
+        element.adjacent.delete(vertex);
+      });
+    }
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) { }
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node }
